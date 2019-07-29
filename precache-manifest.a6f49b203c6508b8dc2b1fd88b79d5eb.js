5f7994321660cebfab38f39ce9478e41837c96dd
satheeshdragon.satheesh@gmail.com <satheeshdragon.satheesh@gmail.com>
2019-07-29 19:37:27 +0530

    Updates

---
+++ precache-manifest.a6f49b203c6508b8dc2b1fd88b79d5eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7572f04a9f3b03b4e1a",
+    "url": "/firebase_crud/static/js/runtime~main.d7572f04.js"
+  },
+  {
+    "revision": "710ea010be38e5c5420b",
+    "url": "/firebase_crud/static/js/main.710ea010.chunk.js"
+  },
+  {
+    "revision": "f286a7522e0b07ffba19",
+    "url": "/firebase_crud/static/js/1.f286a752.chunk.js"
+  },
+  {
+    "revision": "710ea010be38e5c5420b",
+    "url": "/firebase_crud/static/css/main.cfc849d9.chunk.css"
+  },
+  {
+    "revision": "f286a7522e0b07ffba19",
+    "url": "/firebase_crud/static/css/1.e7196d11.chunk.css"
+  },
+  {
+    "revision": "fdeaf6516e3002e3e68e73c3e7f88c8f",
+    "url": "/firebase_crud/index.html"
+  }
+];
